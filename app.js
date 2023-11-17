a84f87e9557e69c69f6832a535131249bf0d8cda
guchur <kdjin11@naver.com>
2023-11-17 17:51:08 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+var mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/dynamic-chat-app');
+
+const app = require('express');
+
+require('http').Server(app)
+
+const http = require('http').Server(app)
+
+http.listen(3000, function(){
+    console.log('Server is running')
+})
