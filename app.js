77ab36ecf201c876d621ec47b8a8c759fbb6f03a
guchur <kdjin11@naver.com>
2023-11-21 20:18:22 +0900

    ✅ [Add] socketio connect & disconnect

--- app.js
+++ app.js
@@ -11,8 +11,29 @@ require('http').Server(app)
 const http = require('http').Server(app)
 
 const userRoute = require('./routers/userRoute')
+const User = require('./models/userModel')
 
 app.use('/', userRoute);
+
+const io = require('socket.io')(http)
+
+var usp =io.of('/user-namespace')
+
+usp.on('connection', async function(socket){
+    console.log('User Connected')
+
+    var userId = socket.handshake.auth.token
+    
+    await User.findByIdAndUpdate({_id:userId}, {$set:{is_online:'1'}})
+
+    socket.on('disconnect', async function(){
+        console.log('user Disconnected')
+        
+        var userId = socket.handshake.auth.token
+        await User.findByIdAndUpdate({_id:userId}, {$set:{is_online:'0'}})
+    
+    })
+})
 
 http.listen(3000, function(){
     console.log('Server is running')
