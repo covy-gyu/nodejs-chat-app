713f24e5159b372b10978362688d41c3e15523f9
guchur <kdjin11@naver.com>
2023-11-22 10:30:39 +0900

    [Add] 유저간 로그인 상태 확인 기능

--- app.js
+++ app.js
@@ -17,24 +17,30 @@ app.use('/', userRoute);
 
 const io = require('socket.io')(http)
 
-var usp =io.of('/user-namespace')
+var usp = io.of('/user-namespace')
 
-usp.on('connection', async function(socket){
+usp.on('connection', async function (socket) {
     console.log('User Connected')
 
     var userId = socket.handshake.auth.token
-    
-    await User.findByIdAndUpdate({_id:userId}, {$set:{is_online:'1'}})
 
-    socket.on('disconnect', async function(){
+    await User.findByIdAndUpdate({ _id: userId }, { $set: { is_online: '1' } })
+
+    //user broadcast online status
+    socket.broadcast.emit('getOnlineUser', { user_id: userId })
+
+    socket.on('disconnect', async function () {
         console.log('user Disconnected')
-        
+
         var userId = socket.handshake.auth.token
-        await User.findByIdAndUpdate({_id:userId}, {$set:{is_online:'0'}})
-    
+        await User.findByIdAndUpdate({ _id: userId }, { $set: { is_online: '0' } })
+
+        //user broadcast online status
+        socket.broadcast.emit('getOfflineUser', { user_id: userId })
+
     })
 })
 
-http.listen(3000, function(){
+http.listen(3000, function () {
     console.log('Server is running')
 })
