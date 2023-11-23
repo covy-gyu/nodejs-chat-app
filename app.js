b0e582d86cf947348940ae72ddd9fbd547141ad7
guchur <kdjin11@naver.com>
2023-11-23 12:37:58 +0900

    ✅ [Add] existsChat, loadChat - 이전 채팅 기록 조회

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const http = require('http').Server(app)
 
 const userRoute = require('./routers/userRoute')
 const User = require('./models/userModel')
+const Chat = require('./models/chatModel')
 
 app.use('/', userRoute);
 
@@ -44,6 +45,18 @@ usp.on('connection', async function (socket) {
     socket.on('newChat', function (data) {
         socket.broadcast.emit('loadNewChat', data)
 
+    })
+
+    //load old chats
+    socket.on('existsChat', async function (data) {
+        var chats = await Chat.find({
+            $or: [
+                { sender_id: data.sender_id, receiver_id: data.receiver_id },
+                { sender_id: data.receiver_id, receiver_id: data.sender_id },
+            ]
+        })
+
+        socket.emit('loadChats', { chats: chats })
     })
 
 })
