3cfb8e5443d1dc2d1ee59b88ccd9cc2d14b2882a
guchur <kdjin11@naver.com>
2023-11-23 11:17:56 +0900

    🗑️ chore

--- models/chatModel.js
+++ models/chatModel.js
@@ -1,21 +1,21 @@
 const mongoose = require('mongoose')
 
 const chatSchema = new mongoose.Schema({
-    sender_id:{
-        type:Schema.Types.ObjectId,
-        ref:'User'
+    sender_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
     },
-    receiver_id:{
-        type:Schema.Types.ObjectId,
-        ref:'User'
+    receiver_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
     },
-    message:{
-        type:String,
-        required:true
+    message: {
+        type: String,
+        required: true
     }
 
 },
-{timestamps:true}
+    { timestamps: true }
 )
 
 module.exports = mongoose.model('Chat', chatSchema)
